99991d61193001b1e19f261c492680e9c6df7e02
Med-94 <mohamedmostapha94@gmail.com>
2018-07-28 20:13:33 +0100

    Add files via upload

---
+++ Mohamed.Programming.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mohamed.Programming
+{
+    class SegmentDisply
+    {
+        Graphics graf;
+
+        static byte[,] bySegment = { 
+        {1,1,1,0,1,1,1},
+        {0,0,1,0,0,1,0}, 
+        {1,0,1,1,1,0,1},
+        {1,0,1,1,0,1,1},
+        {0,1,1,1,0,1,0},
+        {1,1,0,1,0,1,1},
+        {1,1,0,1,1,1,1},
+        {1,0,1,0,0,1,0},
+        {1,1,1,1,1,1,1},
+        {1,1,1,1,0,1,1}};
+
+        readonly Point[][] pt = new Point[7][];
+
+
+        public SegmentDisply(Graphics graf)
+        {
+            this.graf = graf;
+            pt[0] = new Point[] { new Point(3, 2), new Point(39, 2), new Point(31, 10), new Point(11, 10) };
+            pt[1] = new Point[] { new Point(3, 2), new Point(10, 11), new Point(10, 31), new Point(2, 35) };
+            pt[2] = new Point[] { new Point(40, 3), new Point(40,35), new Point(32, 31), new Point(32, 11) };
+            pt[3] = new Point[] { new Point(3, 36), new Point(11, 32), new Point(31, 32), new Point(39, 36),
+                new Point (31,40),new Point (11,40) };
+            pt[4] = new Point[] { new Point(2, 37), new Point(10, 41), new Point(10, 61), new Point(2, 69) };
+            pt[5] = new Point[] { new Point(40, 37), new Point(40,69), new Point(32, 61), new Point(32, 41) };
+            pt[6] = new Point[] { new Point(11, 62), new Point(31, 62), new Point(39, 70), new Point(3, 70) };
+
+        }
+
+        public SizeF MesureString(string str, Font font)
+        {
+            SizeF sizef = new SizeF(0, graf.DpiX * font.SizeInPoints / 72);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                    sizef.Width += 42 * graf.DpiX * font.SizeInPoints / 72 / 72;
+                else if (str[i] == ':')
+                    sizef.Width  += 12 * graf.DpiX * font.SizeInPoints / 72 / 72;
+
+            }
+            return sizef;
+        }
+
+        public void DrawString(string str, Font font, Brush brush, float x, float y)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                    x = Number(str[i] - '0', font, brush, x, y);
+                else if (str[i] == ':')
+                    x = Clone(font, brush, x, y);
+            }
+        }
+
+        float Number(int num, Font font, Brush brush, float x, float y)
+        {
+            for (int i = 0; i < pt.Length; i++)   
+                if (bySegment[num, i] == 1)
+                    Fill(pt[i], font, brush, x, y);
+
+
+            return x + 42 * graf.DpiX * font.SizeInPoints / 72 / 72;          
+        }
+
+        float Clone(Font font, Brush brush, float x, float y)
+        {
+            Point[][] pt = new Point[2][];
+            pt[0] = new Point[] { new Point(2, 21), new Point(6, 17), new Point(10, 21), new Point(6, 25) };
+            pt[1] = new Point[] { new Point(2, 51), new Point(6, 47), new Point(10, 51), new Point(6, 55) };
+
+           for (int i = 0; i < pt.Length; i++)
+                  Fill(pt[i], font, brush, x, y);
+
+
+            return x + 12 * graf.DpiX * font.SizeInPoints / 72 / 72;          
+        }
+          
+       void Fill(Point[] pt, Font font,Brush brush, float x, float y)
+        {
+            PointF[] ptf = new PointF[pt.Length];
+
+            for (int i = 0; i < pt.Length; i++)
+            {
+                ptf[i].X = x + pt[i].X * graf.DpiX * font.SizeInPoints / 72 / 72;
+                ptf[i].Y = y + pt[i].Y * graf.DpiY * font.SizeInPoints / 72 / 72;
+
+            }
+            graf.FillPolygon(brush, ptf);
+        }
+    }
+}
---
+++ Timer_1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Drawing;
+using Mohamed.Programming;
+using Mohamed.Programming.CSharp.Clocks;
+ 
+class Timer_1 : Form
+{
+    DateTime dt;
+    ClockControl clkcon1;
+
+    //public static void Main()
+   // {
+   //     Application.Run(new Timer_1());
+   // }
+
+    Timer_1()
+    {
+        Text = "Timer..";
+        ForeColor = SystemColors.WindowText;  
+        BackColor = Color.White;
+        ResizeRedraw = true;
+        MinimumSize = SystemInformation.MinimizedWindowSize + new Size(0, 1);
+
+        clkcon1 = new ClockControl();
+        clkcon1.Parent = this;
+        clkcon1.Time = DateTime.Now;
+        clkcon1.Dock = DockStyle.None;
+        clkcon1.BackColor = Color.PaleVioletRed;
+        clkcon1.ForeColor = Color.Blue;
+
+        dt = DateTime.Now;
+
+        Timer timer = new Timer();
+        timer.Interval = 1000;
+        timer.Tick += new EventHandler(TimerAnalog);
+        timer.Tick += new EventHandler(TimerOnTick);
+        timer.Enabled = true;
+
+    }
+
+    private void TimerOnTick(object sender, EventArgs ea)
+    {
+        DateTime dtNow = DateTime.Now;
+        dtNow = new DateTime(dtNow.Year, dtNow.Month, dtNow.Day, dtNow.Hour, dtNow.Minute, dtNow.Second);
+        if (dtNow != dt)
+        {
+            dt = dtNow;
+            Invalidate();
+        }
+    }
+
+    private void TimerAnalog(object sender, EventArgs e)
+    {
+        clkcon1.Time = DateTime.Now;
+        
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        SegmentDisply ssd = new SegmentDisply(e.Graphics);
+
+        string strtime = dt.ToString("T", DateTimeFormatInfo.InvariantInfo);
+        SizeF sizef = ssd.MesureString(strtime, Font);
+        float fscal = Math.Min(ClientSize.Width / sizef.Width, ClientSize.Height / sizef.Height);
+        Font font = new Font(Font.FontFamily, fscal * Font.SizeInPoints);
+
+        sizef = ssd.MesureString(strtime, font);
+
+        ssd.DrawString(strtime, font, Brushes.Black, (ClientSize.Width - sizef.Width) / 2, (ClientSize.Height - sizef.Height) / 2);
+
+
+        //Graphics graf = e.Graphics;
+        //String strT = DateTime.Now.ToString("T");
+        //SizeF sif = graf.MeasureString(strT, Font);
+        //float fscal = Math.Min(ClientSize.Width / sif.Width, ClientSize.Height / sif.Height);
+        //Font font = new Font(Font.FontFamily, fscal * Font.SizeInPoints);
+        //sif = graf.MeasureString(strT, font);
+        //graf.DrawString(strT, font, new SolidBrush(ForeColor), (ClientSize.Width - sif.Width) / 2,( ClientSize.Height - sif.Height) / 2);
+
+    }
+
+    void TimerEventH(object obj, EventArgs ea)
+    {
+        Random ran = new Random();
+        int x1 = ran.Next(ClientSize.Width);
+        int x2 = ran.Next(ClientSize.Width);
+        int y1 = ran.Next(ClientSize.Height);
+        int y2 = ran.Next(ClientSize.Height);
+
+        Color color = Color.FromArgb(ran.Next(256),
+                                     ran.Next(256),
+                                     ran.Next(256));
+
+        Graphics graf = CreateGraphics();
+        graf.FillRectangle(new SolidBrush(color),
+                             Math.Min(x1, x2), Math.Min(y1, y2),
+                             Math.Abs(x1- x2), Math.Abs(y1- y2));
+
+        graf.Dispose();
+
+    }
+}
+
